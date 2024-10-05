eebed992879b861bae2dbb4f909b983f1e6aabec
Agus_But <agustinbutrico@gmail.com>
2024-10-05 05:56:02 -0300

    Se añadieron regiones a Subasta

--- Proyecto/LogicaNegocio/Subasta.cs
+++ Proyecto/LogicaNegocio/Subasta.cs
@@ -8,27 +8,34 @@ namespace LogicaNegocio
 {
     public class Subasta : Publicacion
     {
-        // Atributos de la clase
+        #region Atributos de la clase
         private List<Oferta> _ofertas = new List<Oferta>(); // Inicializado con una lista vacía
+        #endregion
 
-        // Propiedades
+        #region Propiedades
         public List<Oferta> Ofertas
         {
             get { return _ofertas; }
             set { _ofertas = value; }
         }
+        #endregion
 
-        // Constructor
+        #region Constructor
         public Subasta(string nombre, string estado, DateTime fecha, List<Articulo> articulos, Cliente cliente, Administrador administrador, DateTime fechaFin, List<Oferta> ofertas)
             : base(nombre, estado, fecha, articulos, cliente, administrador, fechaFin) // Llamada al constructor de la clase base (Usuario)
         {
             Ofertas = ofertas;
         }
+        #endregion
+
+        #region Validación
         // Validación de Subasta, hereda de Publicacion
         public override void Validar()
         {
         }
+        #endregion
 
+        #region Método Equals
         // Sobre escritura del metodo Equals que es usado por Contains
         public override bool Equals(object? obj)
         {
@@ -43,5 +50,6 @@ namespace LogicaNegocio
         {
             return Nombre.GetHashCode();
         }
+        #endregion
     }
 }
